2cc82a2274476119d514e7618d1ac06038645856
Simon Hofmann <simon.hofmann@consol.de>
2019-03-19 00:25:23 +0100

    Exported new function

--- index.spec.ts
+++ index.spec.ts
@@ -1,10 +1,38 @@
+import {Algorithm} from "./lib/algorithm.const";
 import {decrypt} from "./lib/decrypt.function";
 import {encrypt} from "./lib/encrypt.function";
+import {generateKey} from "./lib/generate-key";
 
 describe("secret", () => {
-    it("should return the initial input when passing encrypt to decrytp", async () => {
+    it("should return the initial input when passing encrypt to decrypt", async () => {
         // GIVEN
         const key = "C9HikSYQW/K+ZvRphxEuSw==";
+        const input = "Can you keep a secret?";
+
+        // WHEN
+        const encrypted = await encrypt(input, key);
+        const output = await decrypt(encrypted, key);
+
+        // THEN
+        expect(output).toBe(input);
+    });
+
+    it("should return the initial input when passing encrypt to decrypt with generated key for AES128CBC", async () => {
+        // GIVEN
+        const key = await generateKey(Algorithm.AES128CBC);
+        const input = "Can you keep a secret?";
+
+        // WHEN
+        const encrypted = await encrypt(input, key);
+        const output = await decrypt(encrypted, key);
+
+        // THEN
+        expect(output).toBe(input);
+    });
+
+    it("should return the initial input when passing encrypt to decrypt with generated key for AES256CBC", async () => {
+        // GIVEN
+        const key = await generateKey(Algorithm.AES256CBC);
         const input = "Can you keep a secret?";
 
         // WHEN
--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import {decrypt} from "./lib/decrypt.function";
 import {encrypt} from "./lib/encrypt.function";
+import {generateKey} from "./lib/generate-key";
 
 export const secret = {
     decrypt,
     encrypt,
+    generateKey,
 };
