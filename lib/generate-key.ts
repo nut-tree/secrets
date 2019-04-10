fd45adfcdb22de371fa2fbb16be4d11fe2fb3372
Simon Hofmann <simon.hofmann@consol.de>
2019-04-10 22:17:13 +0200

    (#11) Updated generateKey for new algorithms

--- lib/generate-key.ts
+++ lib/generate-key.ts
@@ -4,10 +4,12 @@ import {generateRandom} from "./random.function";
 export const getKeyLength = (algorithm: Algorithm): { keyLength: number } => {
     switch (algorithm) {
         case Algorithm.AES128CBC:
+        case Algorithm.AES128ECB:
             return ({
                 keyLength: 16,
             });
         case Algorithm.AES256CBC:
+        case Algorithm.AES256ECB:
             return ({
                 keyLength: 32,
             });
