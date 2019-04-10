e447e6102f21caa9ab3d50c09e346f369dae94a6
Simon Hofmann <simon.hofmann@consol.de>
2019-04-10 22:16:17 +0200

    (#11) Updated enum of supported algorithms

--- lib/algorithm.enum.ts
+++ lib/algorithm.enum.ts
@@ -1,4 +1,6 @@
 export enum Algorithm {
     AES128CBC = "aes-128-cbc",
-    AES256CBC = "aes-256-cbc"
+    AES256CBC = "aes-256-cbc",
+    AES128ECB = "aes-128-ecb",
+    AES256ECB = "aes-256-ecb",
 }
