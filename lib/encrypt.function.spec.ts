ba69def8d429177d9e9896cf30331692cd93fed6
Simon Hofmann <simon.hofmann@consol.de>
2019-04-10 22:35:58 +0200

    (#11) Additional tests for AES/ECB encryption

--- lib/encrypt.function.spec.ts
+++ lib/encrypt.function.spec.ts
@@ -13,3 +13,33 @@ describe("secret", () => {
         await expect(() => encrypt(input, key, Algorithm.AES128CBC)).not.toThrow();
     });
 });
+
+describe("AES/ECB", () => {
+    it("should return the correct value for key 'x'", async () => {
+        // GIVEN
+        const keyBuffer = Buffer.alloc(16);
+        Buffer.from("x").copy(keyBuffer);
+        const key = keyBuffer.toString("base64");
+        const input = "Secret text to encrypt";
+
+        // WHEN
+        const encrypted = await encrypt(input, key, Algorithm.AES128ECB);
+
+        // THEN
+        expect(encrypted).toBe("dGxoOqphwaGflpwEstig7SjWzofc65ZJeK7wmTEaeOQ=");
+    });
+
+    it("should return the correct value for key 'encryptor_secret_key'", async () => {
+        // GIVEN
+        const keyBuffer = Buffer.alloc(16);
+        Buffer.from("encryptor_secret_key").copy(keyBuffer);
+        const key = keyBuffer.toString("base64");
+        const input = "Secret text to encrypt";
+
+        // WHEN
+        const encrypted = await encrypt(input, key, Algorithm.AES128ECB);
+
+        // THEN
+        expect(encrypted).toBe("NCC64tvdrOdDJRTAIWOYEz9o2KqukmdFC3eaa7lfqMc=");
+    });
+});
