12152adb5d12f8d7964caad20b7e90cc1b3b717d
Simon Hofmann <simon.hofmann@consol.de>
2019-11-29 15:30:10 +0100

    (#13) Updated imports to only import required parts

--- lib/random.function.ts
+++ lib/random.function.ts
@@ -1,10 +1,10 @@
-import * as crypto from "crypto";
+import {randomBytes} from "crypto";
 
 export const generateRandom = (params: { byteLength: number } = {
     byteLength: 16,
 }): Promise<Buffer> => {
     return new Promise((resolve, reject) => {
-        crypto.randomBytes(params.byteLength, (err, buffer) => {
+        randomBytes(params.byteLength, (err, buffer) => {
             if (err) {
                 reject(err);
             } else {
